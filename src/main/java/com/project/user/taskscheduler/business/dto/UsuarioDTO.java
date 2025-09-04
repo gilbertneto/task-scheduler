bb823c41c2ba29ed8b28466e551678838e195684
Gilberto <gilbertoneto93@gmail.com>
2025-09-04 20:34:10 -0300

    adc configs segurança

---
+++ src/main/java/com/project/user/taskscheduler/business/dto/UsuarioDTO.java
@@ -0,0 +1,18 @@
+package com.project.user.taskscheduler.business.dto;
+
+import lombok.*;
+
+import java.util.List;
+
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
+
+public class UsuarioDTO {
+
+    private String email;
+    private String senha;
+
+}
